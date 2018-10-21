64b5df30ce24d14e51f4896c726d2a6f0adb590b
David Floyd <davidfloyd91@gmail.com>
2018-10-21 16:22:02 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+var number;
+function add(n) {
+  number += n;
+  console.log(number);
+}
+function subtract(n) {
+  number -= n;
+  console.log(number);
+}
+function multiply(n) {
+  number *= n;
+  console.log(number);
+}
+function divide(n) {
+  number /= n;
+  console.log(number);
+}
